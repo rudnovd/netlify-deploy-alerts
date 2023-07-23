297752aef6b2433667f69c9cf500e34777440291
Daniil Rudnov <15265510+rudnovd@users.noreply.github.com>
2023-07-23 21:42:11 +0500

    feat(Targets): add check confirmation modal

--- components/list/Targets.vue
+++ components/list/Targets.vue
@@ -9,8 +9,18 @@
         <span>{{ target.provider }} - {{ target.target }}</span>
 
         <section class="flex gap-1 items-center">
+          <UButton
+            v-if="!target.confirmed"
+            size="xs"
+            color="red"
+            icon="i-heroicons-exclamation-triangle"
+            title="Not confirmed"
+            @click="openConfirmModal(target)"
+          />
+          <UButton v-else disabled size="xs" icon="i-heroicons-check" @click="openEditModal(target)" />
+
           <UButton :disabled="loading" size="xs" icon="i-heroicons-pencil" @click="openEditModal(target)" />
-          <UButton :disabled="loading" size="xs" icon="i-heroicons-trash" @click="deleteModalIsOpen = true" />
+          <UButton :disabled="loading" size="xs" icon="i-heroicons-trash" @click="openDeleteModal(target)" />
         </section>
       </li>
     </ul>
@@ -76,6 +86,43 @@
       </template>
     </UCard>
   </UModal>
+
+  <UModal v-model="confirmModalIsOpen">
+    <UCard>
+      <template #header>
+        <div class="flex items-center justify-between">
+          <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">Confirm target</h3>
+          <UButton
+            color="gray"
+            variant="ghost"
+            icon="i-heroicons-x-mark-20-solid"
+            class="-my-1"
+            @click="confirmModalIsOpen = false"
+          />
+        </div>
+      </template>
+
+      <section>
+        <template v-if="selectedTarget?.provider === 'Telegram'">
+          To allow alerts to be sent to <UBadge color="cyan">{{ selectedTarget.target }}</UBadge
+          >, you must send the <UBadge color="cyan">/start</UBadge> command to
+          <a
+            class="font-medium text-blue-600 dark:text-blue-500 underline"
+            target="_blank"
+            href="https://t.me/netlifydeployalertsbot"
+          >
+            https://t.me/netlifydeployalertsbot
+          </a>
+        </template>
+      </section>
+
+      <template #footer>
+        <section class="flex justify-end gap-2">
+          <UButton :loading="loading" :disabled="loading" @click="checkConfirmation">Check confirmation</UButton>
+        </section>
+      </template>
+    </UCard>
+  </UModal>
 </template>
 
 <script setup lang="ts">
@@ -88,6 +135,7 @@ const targets = useState<Array<Target>>('targets', () => [])
 const loading = ref(false)
 const editModalIsOpen = ref(false)
 const deleteModalIsOpen = ref(false)
+const confirmModalIsOpen = ref(false)
 
 const selectedTarget = ref<Pick<Target, 'id' | 'provider' | 'target'>>({
   id: '',
@@ -102,6 +150,16 @@ const errors = reactive({
 function openEditModal(target: Target) {
   selectedTarget.value = target
   editModalIsOpen.value = true
+}
+
+function openConfirmModal(target: Target) {
+  selectedTarget.value = target
+  confirmModalIsOpen.value = true
+}
+
+function openDeleteModal(target: Target) {
+  selectedTarget.value = target
+  deleteModalIsOpen.value = true
 }
 
 async function editTarget() {
@@ -138,6 +196,26 @@ async function deleteTarget() {
     targets.value = targets.value.filter((s) => s.id !== selectedTarget.value?.id)
     deleteModalIsOpen.value = false
     toast.add({ title: `Target '${selectedTarget.value?.provider} - ${selectedTarget.value?.target}' deleted` })
+  } catch (error) {
+    const err = error as FetchError
+    toast.add({ title: err.message })
+  } finally {
+    loading.value = false
+  }
+}
+
+async function checkConfirmation() {
+  try {
+    loading.value = true
+    const target = await $fetch<Target>(`/api/targets/${selectedTarget.value.id}`)
+    const thisTargetIndex = targets.value.findIndex((t) => t.id === target.id)
+    if (target.confirmed) {
+      targets.value.splice(thisTargetIndex, 1, target)
+      confirmModalIsOpen.value = false
+      toast.add({ title: `Target '${target.target}' confirmed` })
+    } else {
+      toast.add({ title: `Target '${target.target}' not confirmed, use /start command again` })
+    }
   } catch (error) {
     const err = error as FetchError
     toast.add({ title: err.message })
