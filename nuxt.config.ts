13bb6ef382cb14b5fa114004421022b0f4b4051e
Daniil Rudnov <15265510+rudnovd@users.noreply.github.com>
2023-07-30 21:37:42 +0500

    chore(nuxt.config): add runtime config and meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,33 @@ export default defineNuxtConfig({
   nitro: {
     plugins: ['plugins/telegraf.ts'],
   },
+  runtimeConfig: {
+    public: {
+      telegramBotLink: process.env.NUXT_TELEGRAM_BOT_LINK,
+    },
+  },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'Netlify Deploy Alerts',
+      htmlAttrs: {
+        lang: 'en',
+      },
+      meta: [
+        {
+          name: 'application-name',
+          content: 'Netlify Deploy Alerts',
+        },
+        {
+          name: 'description',
+          content: 'Get notifications when your Netlify projects are deployed',
+        },
+        {
+          name: 'keywords',
+          content: 'netlify, deploy, alerts, telegram',
+        },
+      ],
+    },
+  },
 })
