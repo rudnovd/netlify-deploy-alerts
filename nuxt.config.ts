dea1bf5cda61df6f875ce62ce00c20849b141385
rudnovd <rudnov.d@gmail.com>
2023-08-04 23:21:04 +0500

    chore(nuxt.config): add supabase redirect options

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxthq/ui', '@nuxtjs/supabase', '@nuxtjs/eslint-module'],
+  modules: ['@nuxthq/ui', '@nuxtjs/supabase', '@nuxtjs/eslint-module', '@vueuse/nuxt'],
   typescript: {
     strict: true,
   },
@@ -35,6 +35,12 @@ export default defineNuxtConfig({
           content: 'netlify, deploy, alerts, telegram',
         },
       ],
+    },
+  },
+  supabase: {
+    redirectOptions: {
+      login: '/',
+      callback: '/confirm',
     },
   },
 })
